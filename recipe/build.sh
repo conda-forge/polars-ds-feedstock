b9b0d1b14f93ad9eca5b32db60cb0dcdd5ba9ad4
Uwe L. Korn <uwe.korn@quantco.com>
2024-03-25 06:39:24 +0000

    Fix cargo call

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,10 +7,10 @@ if [[ ("${target_platform}" == "win-64" && "${build_platform}" == "linux-64") ]]
   # maturin will expect libpython DSOs at PYO3_CROSS_LIB_DIR
   # which we don't have since we are not able to add python as a host dependency
   cargo add pyo3 \
-      --manifest-path py-polars/Cargo.toml \
+      --manifest-path Cargo.toml \
       --features "abi3-py38,extension-module,multiple-pymethods,generate-import-lib"
   maturin build --release --strip
-  pip install target/wheels/polars*.whl --target $PREFIX/lib/site-packages --platform win_amd64
+  pip install target/wheels/polars-ds*.whl --target $PREFIX/lib/site-packages --platform win_amd64
 else
   # Run the maturin build via pip which works for direct and
   # cross-compiled builds.
