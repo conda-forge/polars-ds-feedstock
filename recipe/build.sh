058f1ed2ebe320fa7cd968434f1af88dcb340c17
Bela Stoyan <bela.stoyan@gmail.com>
2024-04-02 19:52:59 +0300

    try to set abi

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,7 @@ if [[ ("${target_platform}" == "win-64" && "${build_platform}" == "linux-64") ]]
   # we need to add the generate-import-lib feature since otherwise
   # maturin will expect libpython DSOs at PYO3_CROSS_LIB_DIR
   # which we don't have since we are not able to add python as a host dependency
-  cargo feature pyo3 +generate-import-lib
+  cargo feature pyo3 +generate-import-lib +abi3-py38
   maturin build --release --strip
   pip install target/wheels/polars-ds*.whl --target $PREFIX/lib/site-packages --platform win_amd64
 else
