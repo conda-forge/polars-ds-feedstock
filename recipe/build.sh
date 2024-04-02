a781b92b8edcc59f822becd54758189759b61749
Bela Stoyan <bela.stoyan@gmail.com>
2024-04-02 22:03:06 +0300

    fix wheel name

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,7 +8,7 @@ if [[ ("${target_platform}" == "win-64" && "${build_platform}" == "linux-64") ]]
   # which we don't have since we are not able to add python as a host dependency
   cargo feature pyo3 +generate-import-lib +abi3-py38
   maturin build --release --strip
-  pip install target/wheels/polars-ds*.whl --target $PREFIX/lib/site-packages --platform win_amd64
+  pip install target/wheels/polars_ds*.whl --target $PREFIX/lib/site-packages --platform win_amd64
 else
   # Run the maturin build via pip which works for direct and
   # cross-compiled builds.
